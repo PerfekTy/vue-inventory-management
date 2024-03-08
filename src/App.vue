a940a07de9a68aca7218f8a3cb42c5501df26789
Sebastian <sebastianfelenczak@gmail.com>
2024-03-08 19:23:59 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from "./components/ui/button/Button.vue";
+import ModeToggle from "./components/ui/mode-toggle/ModeToggle.vue";
+</script>
+
+<template>
+  <Button>Hello World Shadcn</Button>
+  <ModeToggle />
+</template>
