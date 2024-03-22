ba7db77ef61150a192d75ba3ac0339978ad89e4b
Sebastian <sebastianfelenczak@gmail.com>
2024-03-22 15:53:03 +0100

    added .env file security logic

--- server/db-connection.js
+++ server/db-connection.js
@@ -1,21 +1,13 @@
 const { Pool } = require("pg");
 const db = new Pool({
-  user: "postgres",
-  host: "",
-  database: "inv-managment",
-  password: "postgres",
-  port: "5432",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASWORD,
+  database: process.env.DB_DATABASE_NAME,
+  port: process.env.DB_PORT,
   max: 20,
   idleTimeoutMillis: 30000,
+  host: "",
 });
-
-// pool.query("SELECT * FROM users", (err, result) => {
-//   if (err) {
-//     console.error("Error executing query:", err);
-//   } else {
-//     console.log("Query result:", result.);
-//   }
-// });
 
 module.exports = {
   db,
